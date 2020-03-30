ef66013fafb3c537e33b15dd21bf807d96265ae7
Valsitomo <tomislav.kucanda@gmail.com>
2020-03-30 20:07:24 +0200

    Update Fine-ex.sql

--- SQL scripts/Fine-ex.sql
+++ SQL scripts/Fine-ex.sql
@@ -1,10 +1,15 @@
+CREATE DATABASE FineEx
+GO
+USE FineEx
+GO
+
 CREATE TABLE [Invoice] (
   [IdInvoice] integer PRIMARY KEY IDENTITY(1, 1),
   [Incoming] bit,
-  [CompanyId] integer,
-  [RecepientId] integer,
+  [SenderCompanyId] integer,
+  [RecipientCompanyId] integer,
   [Date] datetime,
-  [Location] nvarchar(255),
+  [LocationId] integer,
   [DueDate] datetime,
   [DeliveryDate] datetime,
   [InvoiceNumber] nvarchar(255),
@@ -17,74 +22,102 @@ CREATE TABLE [Invoice] (
 )
 GO
 
-CREATE TABLE [Recepient] (
-  [Id] nvarchar(255) PRIMARY KEY,
-  [Name] nvarchar(255),
-  [Address] nvarchar(255),
-  [Location] nvarchar(255)
-)
-GO
-
 CREATE TABLE [Item] (
   [IdItem] integer PRIMARY KEY IDENTITY(1, 1),
   [InvoiceId] integer,
   [Name] nvarchar(255),
-  [Price] double,
+  [Price] float,
   [Amount] integer,
   [CompanyId] integer
 )
 GO
 
-CREATE TABLE [Employee] (
-  [IdEmployee] integer PRIMARY KEY IDENTITY(1, 1),
+CREATE TABLE [User] (
+  [IdUser] integer PRIMARY KEY IDENTITY(1, 1),
   [Name] nvarchar(255),
   [Surname] nvarchar(255),
-  [Salary] double,
-  [CompanyId] integer
+  [Salary] float,
+  [CompanyId] integer,
+  [Password] nvarchar(255),
+  [IdCardNumber] nvarchar(255),
+  [IdNumber] nvarchar(255),
+  [IsActive] bit
 )
 GO
 
 CREATE TABLE [Role] (
-  [IdRole] integer PRIMARY KEY IDENTITY(1, 1),
+  [IdRole] integer PRIMARY KEY,
   [Name] nvarchar(255)
 )
 GO
 
 CREATE TABLE [EmployeeRole] (
-  [EmployeeId] integer,
+  [UserId] integer,
   [RoleId] integer
 )
 GO
 
 CREATE TABLE [Company] (
-  [Id] integer PRIMARY KEY,
+  [IdCompany] integer PRIMARY KEY,
   [Name] nvarchar(255),
   [Address] nvarchar(255),
-  [Location] nvarchar(255),
-  [Iban] nvarchar(255)
+  [LocationId] integer,
+  [Iban] nvarchar(255),
+  [Email] nvarchar(255),
+  [IsActive] bit,
+  [InsertDate] datetime,
+  [UpdateDate] datetime,
+  [DeleteDate] datetime
 )
 GO
 
-ALTER TABLE [Invoice] ADD FOREIGN KEY ([CompanyId]) REFERENCES [Company] ([Id])
+CREATE TABLE [Location] (
+  [IdLocation] integer PRIMARY KEY IDENTITY(1, 1),
+  [X] float,
+  [Y] float,
+  [StreetName] nvarchar(255),
+  [StreetNumber] integer,
+  [City] nvarchar(255),
+  [PostalCode] integer,
+  [Country] nvarchar(255)
+)
 GO
 
-ALTER TABLE [Invoice] ADD FOREIGN KEY ([RecepientId]) REFERENCES [Recepient] ([Id])
+ALTER TABLE [Invoice] ADD FOREIGN KEY ([SenderCompanyId]) REFERENCES [Company] ([IdCompany])
 GO
 
-ALTER TABLE [Invoice] ADD FOREIGN KEY ([EmployeeId]) REFERENCES [Employee] ([IdEmployee])
+ALTER TABLE [Invoice] ADD FOREIGN KEY ([RecepientCompanyId]) REFERENCES [Company] ([IdCompany])
+GO
+
+ALTER TABLE [Invoice] ADD FOREIGN KEY ([EmployeeId]) REFERENCES [User] ([IdUser])
+GO
+
+ALTER TABLE [Invoice] ADD FOREIGN KEY ([LocationId]) REFERENCES [Location] ([IdLocation])
+GO
+
+ALTER TABLE [Company] ADD FOREIGN KEY ([LocationId]) REFERENCES [Location] ([IdLocation])
 GO
 
 ALTER TABLE [Item] ADD FOREIGN KEY ([InvoiceId]) REFERENCES [Invoice] ([IdInvoice])
 GO
 
-ALTER TABLE [Invoice] ADD FOREIGN KEY ([CompanyId]) REFERENCES [Company] ([Id])
+ALTER TABLE [Item] ADD FOREIGN KEY ([CompanyId]) REFERENCES [Company] ([IdCompany])
 GO
 
-ALTER TABLE [Employee] ADD FOREIGN KEY ([CompanyId]) REFERENCES [Company] ([Id])
+ALTER TABLE [User] ADD FOREIGN KEY ([CompanyId]) REFERENCES [Company] ([IdCompany])
 GO
 
-ALTER TABLE [EmployeeRole] ADD FOREIGN KEY ([EmployeeId]) REFERENCES [Employee] ([IdEmployee])
+ALTER TABLE [EmployeeRole] ADD FOREIGN KEY ([UserId]) REFERENCES [User] ([IdUser])
 GO
 
 ALTER TABLE [EmployeeRole] ADD FOREIGN KEY ([RoleId]) REFERENCES [Role] ([IdRole])
+GO
+
+INSERT INTO [Role] ([IdRole], [Name]) VALUES
+  (100, 'Site admin'),
+  (90, 'Tech support'),
+  (50, 'Company admin'),
+  (40, 'Sales manager'),
+  (30, 'Procurement manager'),
+  (20, 'Accountant')
 GO
