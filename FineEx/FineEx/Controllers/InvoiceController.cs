95a37dbad6caefb6426d393ca179bd4cd4f035a3
Vilcha <vilim.sepl@gmail.com>
2020-07-05 19:25:38 +0200

    added minor controller changes and ItemViewModel

--- FineEx/FineEx/Controllers/InvoiceController.cs
+++ FineEx/FineEx/Controllers/InvoiceController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FineEx.BusinessLayer.Services.InvoiceService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,29 @@ namespace FineEx.Controllers
 {
     public class InvoiceController : Controller
     {
+        private InvoiceService _invoiceService;
+
+        public InvoiceController()
+        {
+            ViewBag.InvoiceTypes = new SelectList(new List<SelectListItem>
+            {
+                new SelectListItem { Value = "1", Text = "Incoming"},
+                new SelectListItem { Value = "2", Text = "Outgoing"}
+            });
+        }
+
         [HttpGet]
         [Route("invoices")]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Partial(int invoiceType, )
+        {
+
+
         }
     }
 }
