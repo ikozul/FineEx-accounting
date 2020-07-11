8e12a67ee12c46b3ad0e1881dcc6fc02b45dc7dd
Vilcha <vilim.sepl@gmail.com>
2020-07-11 23:47:11 +0200

    added company service to business layer

---
+++ FineEx/FineEx.BusinessLayer/Services/CompanyService/ICompanyService.cs
@@ -0,0 +1,15 @@
+﻿using FineEx.BusinessLayer.Models.CompanyModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FineEx.BusinessLayer.Services.CompanyService
+{
+    public interface ICompanyService
+    {
+        List<CompanyViewModel> GetCompanies();
+        void GetCompanyViewModels();
+    }
+}
