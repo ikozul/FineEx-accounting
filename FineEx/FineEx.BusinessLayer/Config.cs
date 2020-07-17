b25171d3fadfdcc43b46d47acccf1678aa37fb6f
Ivan <ivan.kozul@edge-group.info>
2020-07-17 18:15:15 +0200

    Added generator

--- FineEx/FineEx.BusinessLayer/Config.cs
+++ FineEx/FineEx.BusinessLayer/Config.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Configuration;
 
 namespace FineEx.BusinessLayer
 {
-    class Config
+    public static class Config
     {
         // temp
         public const string Salt = "(*.*)Fine(*_*)Ex_(*o*)_";
+        public static string  PdfPath = WebConfigurationManager.AppSettings["PFUserName"];
     }
 }
---
+++ FineEx/FineEx.BusinessLayer/Services/PdfGenerator/Model/QuickResponseCode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FineEx.BusinessLayer.Services.PdfGenerator.Model
+{
+    internal class QuickResponseCode
+    {
+        //TODO public TYPE Type { get; set; }
+    }
+}
