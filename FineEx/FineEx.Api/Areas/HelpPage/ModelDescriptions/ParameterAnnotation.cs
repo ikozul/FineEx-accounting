cc39275022b71ea9289d7f2f534d305b566ff945
Ivan <ivan.kozul@edge-group.info>
2020-07-21 20:33:21 +0200

    Added web.api

---
+++ FineEx/FineEx.Api/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FineEx.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
