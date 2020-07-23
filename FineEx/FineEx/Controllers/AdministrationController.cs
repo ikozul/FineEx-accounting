7d2c262310f65c4ea3319d2abd41143f950968da
Ivan <ivan.kozul@edge-group.info>
2020-07-23 20:05:07 +0200

    refactored export for expoadability

--- FineEx/FineEx/Controllers/AdministrationController.cs
+++ FineEx/FineEx/Controllers/AdministrationController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using CsvHelper;
 using FineEx.BusinessLayer.Models.InvoiceModels;
 using FineEx.BusinessLayer.Services.Billing;
+using FineEx.BusinessLayer.Services.Export;
 using FineEx.DataLayer.Context;
 
 namespace FineEx.Controllers
@@ -28,32 +29,15 @@ namespace FineEx.Controllers
                 BillingService service = new BillingService(model.Id);
                 service.BillCompany();
             }
+
             App.Db.SaveChanges();
             return RedirectToAction("Index", "Administration");
-
         }
 
         public FileContentResult ExportData()
         {
-            using (var memoryStream = new MemoryStream())
-            {
-                using (TextWriter writer = new StreamWriter(memoryStream))
-                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-                {
-                    csv.Configuration.Delimiter = ";";
-                    foreach (var invoice in App.Db.Invoices.Where(x => x.SenderId == 1))
-                    {
-                        var invoiceViewModel = new InvoiceViewModel(invoice);
-                        csv.WriteRecord(invoiceViewModel);
-                        csv.NextRecord();
-                    }
-
-                    writer.Flush();
-                    csv.Flush();
-
-                    return File(fileContents: memoryStream.GetBuffer(), "text/csv", "export.csv");
-                }
-            }
+            var invoiceExport = new ExportInvoices(1);
+            return File(fileContents: invoiceExport.ExportData(), "text/csv", "export.csv");
         }
     }
 }
